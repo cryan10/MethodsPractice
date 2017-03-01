07ba5ef90501f3de02f5f6f5cd589c9edba6013b
Carrie Ryan <cryan1205@gmail.com>
2017-02-28 20:21:58 -0500

    1st commit

---
+++ MethodsPractice/MethodsPractice/Program.cs
@@ -0,0 +1,81 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi();
+        }
+
+        static void SayHi()
+        {
+            Console.WriteLine("hi");
+        }
+    }
+}
+*/
+
+/* using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer: ");
+            int numberOne = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter another integer: ");
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine("The sum of these numbers is " + Add(numberOne, numberTwo));
+        }
+
+        static int Add(int numberOne, int numberTwo)
+        {
+            int sum = numberOne + numberTwo;
+            return sum;
+        }
+    }
+}
+*/
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name: ");
+            string userName = Console.ReadLine();
+            Greeting(userName);
+            
+        }
+
+
+
+        static void Greeting(string userName)
+        { 
+            Console.WriteLine("Hello, " + userName);
+        }
+    }
+}
+*/
